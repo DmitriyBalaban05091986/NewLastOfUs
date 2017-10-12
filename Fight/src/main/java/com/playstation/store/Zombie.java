05359af288f84bad5467ad518635f5f23c2ee6ba
Ivan <dbalaban@ukr.net>
2017-10-13 00:59:49 +0300

    Zombie v2.1.2

--- Fight/src/main/java/com/playstation/store/Zombie.java
+++ Fight/src/main/java/com/playstation/store/Zombie.java
@@ -1,6 +1,7 @@
 package com.playstation.store;
 
 public class Zombie {
+	//test comment for zombie commit
 	public void eatHuman(Human human) {
 		System.out.printf("Human has been eaten:\n his Name was: %s | and his Weight was: %d\n", human.getName(),
 				human.getWeight());
