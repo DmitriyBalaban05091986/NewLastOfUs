dcb84aeac13322f4119e1577c3ad0ff0b798af8e
Ivan <dbalaban@ukr.net>
2017-10-13 00:42:13 +0300

    Fight v2

--- Fight/src/main/java/com/playstation/store/Fight.java
+++ Fight/src/main/java/com/playstation/store/Fight.java
@@ -8,7 +8,8 @@ public class Fight
 {
     public static void main( String[] args )
     {
-        Zombie zombie = new Zombie();
+        //This is a test change for git task
+    	Zombie zombie = new Zombie();
         zombie.eatHuman(new Human("Ivan Petrovich", 50));
   
     }
